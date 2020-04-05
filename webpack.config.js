d8e38cc9958244859daac8fce5e4d096cf69dd94
artem-kochetkov-94 <artem-kochetkov-94@mail.ru>
2020-04-05 17:59:02 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
