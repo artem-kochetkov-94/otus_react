0fb312c6f7c0b15d1d9bbe181a3584c034762d6d
artem-kochetkov-94 <artem-kochetkov-94@mail.ru>
2020-05-20 13:06:43 +0300

    added exit button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import { logout } from 'src/api/auth'
 
 export const Header: React.FC<{}> = () => {
   const login = localStorage.getItem('login')
+  const history = useHistory()
+  const onSubmit = useCallback(async (ev) => {
+    ev.preventDefault()
+    await logout()
+    history.push(`/login`)
+  }, [])
 
   return (
     <div>
       <div>User: {login}</div>
+      <form onSubmit={onSubmit}>
+        <button type="submit">Выйти</button>
+      </form>
     </div>
   )
 }
