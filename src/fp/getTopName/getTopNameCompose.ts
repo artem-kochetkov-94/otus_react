9614a61f4863023262a91dec3ea0ba463cd91b82
artem-kochetkov-94 <artem-kochetkov-94@mail.ru>
2020-05-25 23:16:42 +0300

    getTopNameCompose

--- src/fp/getTopName/getTopNameCompose.ts
+++ src/fp/getTopName/getTopNameCompose.ts
@@ -1,11 +1,9 @@
 import { Team } from './'
-import { compose } from 'ramda'
+import { compose, reduce } from 'src/utils'
 
-const getTopTeam = (teams: Team[]): Team =>
-  teams.reduce((accomulator: Team, team: Team) =>
-    team.score > accomulator.score ? team : accomulator,
-  )
+const getTopTeam = (accomulator: Team, team: Team) =>
+  team.score > accomulator.score ? team : accomulator
 
 const getTopTeamName = (team: Team): string => team.name
 
-export const getTopNameCompose = compose(getTopTeamName, getTopTeam)
+export const getTopNameCompose = compose(getTopTeamName, reduce(getTopTeam))
---
+++ src/utils/index.ts
@@ -0,0 +1,4 @@
+export * from './compose'
+export * from './reduce'
+export * from './numbersToWords'
+export * from './sleep'
